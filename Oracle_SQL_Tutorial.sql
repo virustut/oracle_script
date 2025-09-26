489bcd3cca5966dd463c1986a6c5e2462742a0d6
virustut <163240194+virustut@users.noreply.github.com>
2025-09-26 12:04:09 +0530

    Create Oracle_SQL_Tutorial.sql

---
+++ Oracle_SQL_Tutorial.sql
@@ -0,0 +1,18 @@
+------- Here we have learn Oracle_SQL_Tutorial From Scratch.
+
+Install Oracle 21c 
+Install Oracle Devoloper for running query 
+
+--------Data Type in ORACLE--------
+Number --> when we use number 
+Char -->char  (fixed in size)
+     -->varchar2 (don't know the size)
+     -->varchar 
+     -->nvarchar2 
+     -->Ncharvarchar2 
+Long  --(mixed of number and character) around 2gb of data particular row or column
+Date
+RAW  -->NCLOB
+     -->BLOB
+     -->CLOB
+RAWID -- is nothing but it's a used of identification and it is a hidden column 
